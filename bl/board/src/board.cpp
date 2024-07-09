d203df48e082d31e1153a46d938805bb746a06d8
kecajjo <58090327+kecajjo@users.noreply.github.com>
2024-07-09 11:31:11 +0200

    fix clang-tidy errors (#5)
    * fix clang-tidy errors
    
    * return const reference to unordered map in drawable

--- bl/board/src/board.cpp
+++ bl/board/src/board.cpp
@@ -1,7 +1,6 @@
 #include "board.hpp"
 
-void Board::addBlock(std::shared_ptr<Block> blck, Drawable::Position p)
-{
+void Board::addBlock(const std::shared_ptr<Block> &blck, Drawable::Position p) {
     blocks.insert(blck);
     addChild(blck, p);
 }
