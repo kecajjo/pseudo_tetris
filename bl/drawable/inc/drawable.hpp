6da9237577c274fc640636feb66073db8d1c954b
Jacek Multan <kecajorigami@gmail.com>
2024-07-11 18:13:12 +0200

    clang-tidy fixes

--- bl/drawable/inc/drawable.hpp
+++ bl/drawable/inc/drawable.hpp
@@ -3,24 +3,26 @@
 #include <limits>
 #include <list>
 #include <memory>
-#include <tuple>
 #include <unordered_map>
-#include <utility>
+
+// TODO temp struct, will be removed in the next PR
+struct Color{int r,g,b,a;
+Color() : r(0), g(0), b(0), a(0) {}
+    Color(const uint8_t& red, const uint8_t& green, const uint8_t& blue, const uint8_t& alpha)
+        : r(red), g(green), b(blue), a(alpha) {}};
 
 class Drawable {
    public:
     struct Position {
         int x;
         int y;
-    };
-    struct Color {
-        uint8_t r, g, b, a;
+        Position operator+(const Position &p) const;
     };
 
-    explicit Drawable(const Drawable &d);
-    virtual Drawable &operator=(const Drawable &d) = default;
+    explicit Drawable(const Drawable &d) = default;
+    Drawable &operator=(const Drawable &d) = default;
     explicit Drawable(Drawable &&d) noexcept;
-    virtual Drawable &operator=(Drawable &&d) noexcept;
+    Drawable &operator=(Drawable &&d) noexcept;
     virtual ~Drawable() = default;
 
     std::list<Position> getShape() const { return shape; };
@@ -29,14 +31,18 @@ class Drawable {
     const std::unordered_map<std::shared_ptr<Drawable>, Drawable::Position> &getChildDrawables()
         const;
     void addChild(const std::shared_ptr<Drawable> &child, Position p);
+    void removeChild(const std::shared_ptr<Drawable> &child);
+    void clearChildren();
+    void setColor(const Color &c) { color = c; }
 
    protected:
     explicit Drawable() = default;
 
-    Color color = {max_color_val, max_color_val, max_color_val, max_color_val};
+    // NOLINTNEXTLINE
     std::list<Position> shape;
-    std::unordered_map<std::shared_ptr<Drawable>, Position> children;
 
    private:
+    std::unordered_map<std::shared_ptr<Drawable>, Position> children;
+    Color color = {max_color_val, max_color_val, max_color_val, max_color_val};
     static constexpr unsigned max_color_val = std::numeric_limits<unsigned char>::max();
 };
--- bl/drawable/src/drawable.cpp
+++ bl/drawable/src/drawable.cpp
@@ -9,21 +9,23 @@ void Drawable::addChild(const std::shared_ptr<Drawable> &child, Drawable::Positi
     children[child] = p;
 }
 
-Drawable::Drawable(const Drawable &d) {
-    color = d.color;
-    shape = d.shape;
-    children = d.children;
-}
+void Drawable::removeChild(const std::shared_ptr<Drawable> &child) { children.erase(child); }
 
-Drawable::Drawable(Drawable &&d) noexcept {
-    color = d.color;
-    shape = std::move(d.shape);
-    children = std::move(d.children);
-}
+void Drawable::clearChildren() { children.clear(); }
+
+Drawable::Drawable(Drawable &&d) noexcept
+    : shape(std::move(d.shape)), children(std::move(d.children)), color(d.color) {}
 
 Drawable &Drawable::operator=(Drawable &&d) noexcept {
     color = d.color;
     shape = std::move(d.shape);
     children = std::move(d.children);
     return *this;
+}
+
+Drawable::Position Drawable::Position::operator+(const Position &p) const {
+    Position retVal{};
+    retVal.x = x + p.x;
+    retVal.y = y + p.y;
+    return retVal;
 }
