a87154d8938fd85e5fb4d2fb3798860d40274946
Jacek Multan <kecajorigami@gmail.com>
2024-07-12 10:16:15 +0200

    fix review comments

--- bl/drawable/inc/drawable.hpp
+++ bl/drawable/inc/drawable.hpp
@@ -3,22 +3,18 @@
 #include <limits>
 #include <list>
 #include <memory>
+#include <tuple>
 #include <unordered_map>
-
-// TODO temp struct, will be removed in the next PR
-struct Color {
-    int r, g, b, a;
-    Color() : r(0), g(0), b(0), a(0) {}
-    Color(const uint8_t &red, const uint8_t &green, const uint8_t &blue, const uint8_t &alpha)
-        : r(red), g(green), b(blue), a(alpha) {}
-};
+#include <utility>
 
 class Drawable {
    public:
     struct Position {
         int x;
         int y;
-        Position operator+(const Position &p) const;
+    };
+    struct Color {
+        uint8_t r, g, b, a;
     };
 
     explicit Drawable(const Drawable &d) = default;
@@ -33,18 +29,17 @@ class Drawable {
     const std::unordered_map<std::shared_ptr<Drawable>, Drawable::Position> &getChildDrawables()
         const;
     void addChild(const std::shared_ptr<Drawable> &child, Position p);
-    void removeChild(const std::shared_ptr<Drawable> &child);
-    void clearChildren();
-    void setColor(const Color &c) { color = c; }
 
    protected:
     explicit Drawable() = default;
 
-    // NOLINTNEXTLINE
+    // TODO change in the drawing engine PR
+    // NOLINTBEGIN
+    Color color = {max_color_val, max_color_val, max_color_val, max_color_val};
     std::list<Position> shape;
+    std::unordered_map<std::shared_ptr<Drawable>, Position> children;
+    // NOLINTEND
 
    private:
-    std::unordered_map<std::shared_ptr<Drawable>, Position> children;
-    Color color = {max_color_val, max_color_val, max_color_val, max_color_val};
     static constexpr unsigned max_color_val = std::numeric_limits<unsigned char>::max();
 };
